ae9b5224986ae12cfc5581fe43da192436306e03
Yuno0205 <mainhathao195@gmail.com>
2025-12-10 08:09:55 +0700

    change to exactly quality of image

--- components/StoryBlok/bloks/Exploration.tsx
+++ components/StoryBlok/bloks/Exploration.tsx
@@ -22,7 +22,7 @@ export default function Exploration({ blok }: { blok: ExplorationBlok }) {
               alt={blok.background_image.alt || blok.title}
               fill
               className="w-full object-cover h-full"
-              quality={85}
+              quality={75}
               sizes="100vw"
             />
           )}
