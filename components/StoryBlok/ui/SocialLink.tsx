143901f997bb24fb99529ca193d5f36df466cdb5
Yuno0205 <mainhathao195@gmail.com>
2025-12-17 11:51:26 +0700

    avoid CLS in footer

--- components/StoryBlok/ui/SocialLink.tsx
+++ components/StoryBlok/ui/SocialLink.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { getStoryblokAssetDimensions } from "@/lib/utils";
 import { SocialLinkStoryblok } from "@/types/storyblok";
 import { storyblokEditable } from "@storyblok/react";
 import Image from "next/image";
@@ -10,7 +9,6 @@ export default function SocialLink({ blok }: { blok: SocialLinkStoryblok }) {
   const isExternal = blok.link.linktype === "url";
 
   const href = blok.link.cached_url || "/";
-  const iconDimensions = getStoryblokAssetDimensions(blok.icon?.filename);
 
   return (
     <Link
@@ -22,8 +20,8 @@ export default function SocialLink({ blok }: { blok: SocialLinkStoryblok }) {
       <Image
         src={blok.icon.filename || ""}
         alt={blok.icon.alt || "Social Icon"}
-        width={iconDimensions?.width ?? 36}
-        height={iconDimensions?.height ?? 36}
+        width={36}
+        height={36}
       />
     </Link>
   );
